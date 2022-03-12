f642acb1c8e1342ebb33c786dfd0a8aad4332c0e
bhargav <bhargav2009narola@gmail.com>
2022-03-12 16:41:05 +0530

    Chapter 1

---
+++ lib/widgets/item_widget.dart
@@ -0,0 +1,32 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item? item;
+
+  const ItemWidget({Key? key, required this.item})
+      : assert(item != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Image.network(item!.image.toString()),
+        title: Text(item!.name.toString()),
+        subtitle: Text(item!.desc.toString()),
+        trailing: Text(
+            "\$${item!.price.toString()}",
+            textScaleFactor: 1.5,
+            style: TextStyle(
+              color: Colors.deepPurple,
+              fontWeight: FontWeight.bold
+            ),
+
+        ),
+      ),
+    );
+  }
+}
